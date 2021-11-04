803a99b5c0dd2ce5d245f8fb9088f9cadce9a835
Mohamed-Sonbol <mohamedsonbol12@gmail.com>
2021-11-04 23:26:57 +0200

    All functionalities done except routing and search

--- client/jett-airlines/src/components/FlightDetails.js
+++ client/jett-airlines/src/components/FlightDetails.js
@@ -7,20 +7,43 @@ import Typography from '@mui/material/Typography';
 
 
 export default function FlightDetails(props) {
+    const flight=props.flight
     return (
         <div>
-            <Card sx={{ maxWidth: 350 }}>
+            <Card sx={{ maxWidth: 500 }}>
                 <CardActionArea>
-                    <AirplaneTicketIcon color="primary" sx={{ fontSize: 100 }} />
+                    <AirplaneTicketIcon color="primary" sx={{ fontSize: 150 }} />
                     <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                            Flight Number: {props.flight.flightNumber}
+                    <Typography gutterBottom variant="h5" sx={{ fontStyle: 'oblique' }}>
+                            Flight Number: {flight.flightNumber}
+                            <br/>
+                            Terminal: {flight.airportTerminal}
+                            <br/>
+                            From: {flight.from}                         
+                            <br/>
+                            To: {flight.to}
+                            <br/>
+                            Departure Date: {flight.departureDate}
+                            <br/>
+                            Arrival Date: {flight.arrivalDate}
                         </Typography>
-                        <Typography gutterBottom variant="h5" component="div">
-                            From {props.flight.from} To {props.flight.to}
-                        </Typography>
+                        <p>-------------------------------------------------------------------------------------</p>
                         <Typography variant="body2" color="text.secondary">
-                            {props.flight.departureDate}
+                            Economy Seats: {flight.economySeats}
+                            <br/>
+                            Business Seats: {flight.businessSeats}
+                            <br/>
+                            First Class Seats: {flight.firstSeats}
+                            <br/>
+                            Total Number Seats: {flight.totalSeats}
+                            <br/>
+                            Price of Economy: {flight.economyPrice}
+                            <br/>
+                            Price of Business: {flight.businessPrice}
+                            <br/>
+                            Price of First Class: {flight.firstPrice}
+                            <br/>
+                            Baggage Allowance: {flight.baggageAllowance}
                         </Typography>
                     </CardContent>
                 </CardActionArea>
