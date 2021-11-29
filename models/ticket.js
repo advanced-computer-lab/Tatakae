47db03ff4846b3caae99294fc72d0ff291314173
Mahmoud-Hesham99 <mhm1h9@gmail.com>
2021-11-29 21:46:17 +0200

    editing UI

--- models/ticket.js
+++ models/ticket.js
@@ -14,6 +14,10 @@ const ticketSchema = new mongoose.Schema({
     type: mongoose.ObjectId,
     required: true
   },
+  email:{
+    type: String,
+    required: true
+  },
   flight:{
     type: mongoose.ObjectId,
     required: true
