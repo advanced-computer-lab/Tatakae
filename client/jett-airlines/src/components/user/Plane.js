78cfa323074a14e08c76f5dc456091428aaf5eb7
YoussefAswad55 <85849220+YoussefAswad55@users.noreply.github.com>
2021-11-30 01:05:17 +0200

    Plane view nearly done

--- client/jett-airlines/src/components/user/Plane.js
+++ client/jett-airlines/src/components/user/Plane.js
@@ -1,13 +1,27 @@
-import { Grid, Box} from '@mui/material';
+import React from 'react'
+import { Grid, Box, List, ListItem, Radio, RadioGroup, FormControlLabel, FormControl, FormLabel } from '@mui/material';
 import Seat from './Seat'
+import '../../css/Plane.css'
+import { useEffect } from 'react';
 
-let economySeats = [Boolean(true), Boolean(false), Boolean(true), Boolean(true), Boolean(false), Boolean(true)]
+let economySeats = [Boolean(true), Boolean(false), Boolean(true), Boolean(true), Boolean(false), Boolean(true), Boolean(false), Boolean(true)]
 
-let businessSeats = [Boolean(true), Boolean(false), Boolean(true), Boolean(true), Boolean(true), Boolean(true)]
+let businessSeats = [Boolean(true), Boolean(false), Boolean(true), Boolean(true), Boolean(true), Boolean(true), Boolean(false), Boolean(true)]
 
-let firstSeats = [Boolean(true), Boolean(false), Boolean(true), Boolean(true), Boolean(false), Boolean(false)]
+let firstSeats = [Boolean(true), Boolean(false), Boolean(true), Boolean(true), Boolean(false), Boolean(false), Boolean(false), Boolean(true)]
 
-let seatWidth = 3;
+let economyPrice = 1000;
+
+let firstPrice = 2000;
+
+let businessPrice = 3000;
+
+const colors = {
+    availableColor: "green",
+    selectedColor: "blue",
+    occupiedColor: "grey"
+
+}
 
 const styles = {
     bSeatStyle: {
@@ -17,43 +31,108 @@ const styles = {
         margin: "1em",
         borderTopLeftRadius: "10px",
         borderTopRightRadius: "10px"
-        },
+    },
     fSeatStyle: {
         backgroundColor: "purple"
     },
     eSeatStyle: {
         backgroundColor: "red"
     },
-    oSeatStyle:{
+    oSeatStyle: {
         backgroundColor: "grey"
     }
 }
 
+
+
 export default function Plane(props) {
 
-    const getWidth = (seatNumber) => {
-        if(seatNumber === 2)
-            return 3;
-        return 1;
+    const [selected, setSelected] = React.useState(0);
+
+    const [totalPrice, setTotalPrice] = React.useState(0);
+
+    const [isChild, setIsChild] = React.useState(false);
+
+    const splitArray = (seatArray) => {
+        var rows = [[]];
+        for (var i = 0; i < seatArray.length; i += 8) {
+            rows.push(seatArray.slice(i, i + 8))
+        }
+        return rows;
+
     }
 
+    const handleRadio = (event) => {
+        if(event.target.value === "adult"){
+            setIsChild(false);
+        }
+        else {
+            setIsChild(true);
+        }
+    }
+
+    useEffect(() => { }, [selected] , [totalPrice], [isChild])
+
     return (
-        <Grid container columns="8" wrap="wrap" sx={{maxWidth: "50%"}}>
-            {businessSeats.map( (available, seatNumber) => (
-                <Grid item xs={getWidth(seatNumber)}>
-                <Seat available={available} type="b" seatNumber={seatNumber}/>
+        <Grid>
+            <Grid class="plane-container">
+                <List class="showcase">
+                    <ListItem>
+                        <Grid class="seat" style={{ background: colors.availableColor }}></Grid>
+                        <small>Available</small>
+                    </ListItem>
+                    <ListItem>
+                        <Grid class="seat selected" style={{ background: colors.selectedColor }}></Grid>
+                        <small>Selected</small>
+                    </ListItem>
+                    <ListItem>
+                        <Grid class="seat occupied" style={{ background: colors.occupiedColor }}></Grid>
+                        <small>Occupied</small>
+                    </ListItem>
+                </List>
+
+                <FormControl onChange={handleRadio} component="fieldset">
+                    <RadioGroup defaultValue="adult" row aria-label="age" name="row-radio-buttons-group">
+                        <FormControlLabel value="adult" control={<Radio />} label="Adult" />
+                        <FormControlLabel value="child" control={<Radio />} label="Child" />
+                    </RadioGroup>
+                </FormControl>
+
+                <Grid class="container">
+                    Business
+                    {splitArray(businessSeats).map(row => (
+                        <Grid class="row">
+
+                            {row.map((element, seatNumber) => (
+                                <Seat isChild={isChild} price={businessPrice} totalPrice={totalPrice} setTotalPrice={setTotalPrice} available={element} colors={colors} seatNumber={seatNumber} selected={selected} setSelected={setSelected} />
+                            ))}
+                        </Grid>
+                    ))}
+                    First
+                    {splitArray(firstSeats).map(row => (
+                        <Grid class="row">
+
+                            {row.map((element, seatNumber) => (
+                                <Seat isChild={isChild} price={firstPrice} totalPrice={totalPrice} setTotalPrice={setTotalPrice} available={element} colors={colors} seatNumber={seatNumber} selected={selected} setSelected={setSelected} />
+                            ))}
+                        </Grid>
+                    ))}
+                    Economy
+                    {splitArray(economySeats).map(row => (
+                        <Grid class="row">
+
+                            {row.map((element, seatNumber) => (
+                                <Seat isChild={isChild} price={economyPrice} totalPrice={totalPrice} setTotalPrice={setTotalPrice} available={element} colors={colors} seatNumber={seatNumber} selected={selected} setSelected={setSelected} />
+                            ))}
+                        </Grid>
+                    ))}
+
+
+                    <p class="text">
+                        You have selected <span>{selected}</span> seats for the total price of <span id="total">${totalPrice}</span>
+                    </p>
                 </Grid>
-            ))}
-            {firstSeats.map( (available, seatNumber) => (
-                <Grid item xs={getWidth(seatNumber)}>
-                <Seat available={available} type="b" seatNumber={seatNumber}/>
-                </Grid>
-            ))}
-            {economySeats.map( (available, seatNumber) => (
-                <Grid item xs={getWidth(seatNumber)}>
-                <Seat available={available} type="b" seatNumber={seatNumber}/>
-                </Grid>
-            ))}
+            </Grid>
         </Grid>
     )
 }
--- client/jett-airlines/src/components/user/Seat.js
+++ client/jett-airlines/src/components/user/Seat.js
@@ -1,22 +1,66 @@
-import { Grid, Box} from '@mui/material';
+import React from 'react'
+import { Grid, Box } from '@mui/material';
+import '../../css/Plane.css'
+import { useEffect } from 'react';
 
 export default function Seat(props) {
-
-    let seatColor;
-    if(props.available){
-        switch(props.type){
-            case "b": seatColor = { backgroundColor: "blue" };break;
-            case "f": seatColor = { backgroundColor: "purple" };break;
-            case "e": seatColor = { backgroundColor: "black" };break;
-            default: seatColor = { backgroundColor: "green" };
+    const [refresh, setRefresh] = React.useState(false);
+    //const [pressed, setPressed] = React.useState(false);
+    const [pressed, setPressed] = React.useState(0);
+    let seatColor = { backgroundColor: props.colors.selectedColor };
+    let className = "seat";
+    if (props.available) {
+        if (pressed === 0) {
+            seatColor = { backgroundColor: props.colors.availableColor };
         }
     }
-    else
-        seatColor = { backgroundColor: "grey" }
+    else {
+        seatColor = { backgroundColor: props.colors.occupiedColor }
+        className += " occupied";
+    }
+
+    const handleClick = () => {
+        if (props.available) {
+            if (pressed !== 0) {
+                props.setSelected(props.selected - 1)
+                if (pressed === 1) {
+                    props.setTotalPrice(props.totalPrice - props.price)
+                    setPressed(0);
+                } else if (pressed === 2) {
+                    props.setTotalPrice(props.totalPrice - (0.5 * props.price))
+                    setPressed(0);
+                } else {
+                    if (props.isChild) {
+                        props.setTotalPrice(props.totalPrice - (0.5 * props.price))
+                    }
+                    else {
+                        props.setTotalPrice(props.totalPrice - props.price)
+                    }
+                }
+            }
+            else {
+                props.setSelected(props.selected + 1)
+                if (props.isChild) {
+                    setPressed(2);
+                    props.setTotalPrice(props.totalPrice + (0.5 * props.price))
+                }
+                else {
+                    setPressed(1);
+                    props.setTotalPrice(props.totalPrice + props.price)
+                }
+            }
+
+            //setPressed(!pressed);
+            setRefresh(!refresh)
+        }
+
+    }
+
+    useEffect(() => { }, [refresh])
 
     return (
-        <Box style={seatColor} sx={{textAlign: "center", borderRadius: "1em 1em 0em 0em"}}>
+        <Grid onClick={handleClick} class={className} style={seatColor}>
             {props.seatNumber}
-        </Box>
+        </Grid>
     )
 }
