4811e460dbc59abbd34ac5f2d3cd3dfc95e86bf7
Mohamed-Sonbol <mohamedsonbol12@gmail.com>
2021-12-04 03:06:56 +0200

    TicketBoard done with routing TicketCard component remainig

--- client/jett-airlines/src/index.js
+++ client/jett-airlines/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import {BrowserRouter} from 'react-router-dom';
+import TicketBoard from './components/user/TicketBoard';
 
 ReactDOM.render(<BrowserRouter>
 <App />
