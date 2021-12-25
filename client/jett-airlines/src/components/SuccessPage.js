120e20c2c5ac2f464f7c59a35bd8ed132f024457
Arc0fLife <ahmedyehia3633@yahoo.com>
2021-12-25 23:04:12 +0300

    Co-authored-by: youssefaswad@outlook.com <youssefaswad@outlook.com> Co-authored-by: mostafamohamedaly <mostafamohamedaly@users.noreply.github.com> Co-authored-by: YoussefAswad <YoussefAswad@users.noreply.github.com> Co-authored-by: Mohamed-Sonbol <Mohamed-Sonbol@users.noreply.github.com>

--- client/jett-airlines/src/components/SuccessPage.js
+++ client/jett-airlines/src/components/SuccessPage.js
@@ -33,6 +33,96 @@ export default function SuccessPage() {
             sessionStorage.removeItem('returnData')
             sessionStorage.removeItem('returnDataBooks')
         }
+        /* const deptTicket = {
+            flight: flight._id,
+            from: flight.from,
+            to: flight.to,
+            departureTerminal: flight.departureTerminal,
+            arrivalTerminal: flight.arrivalTerminal,
+            departureDate: new Date(flight.departureDate),
+            arrivalDate: new Date(flight.arrivalDate),
+            economySeatsAdults: economySeatsAdults,
+            businessSeatsAdults: businessSeatsAdults,
+            firstSeatsAdults: firstSeatsAdults,
+            economySeatsChildren: economySeatsChildren,
+            businessSeatsChildren: businessSeatsChildren,
+            firstSeatsChildren: firstSeatsChildren,
+            totalPrice: totalPrice
+
+            const dataBooks = {
+      token: sessionStorage.getItem('token'),
+      economySeatsAdults: economySeatsAdults,
+      businessSeatsAdults: businessSeatsAdults,
+      firstSeatsAdults: firstSeatsAdults,
+      economySeatsChildren: economySeatsChildren,
+      businessSeatsChildren: businessSeatsChildren,
+      firstSeatsChildren: firstSeatsChildren,
+      flightId: flight._id
+    }
+        }*/
+        const prevTicket = JSON.parse(sessionStorage.getItem('Ticket'));
+        const newTicket = JSON.parse(sessionStorage.getItem('newTicket'));
+
+        if (prevTicket) {
+            const token_allold_seatlists_flightid = {
+                token: sessionStorage.getItem('token'),
+                flightId: prevTicket.flight,
+                firstSeatsAdults: prevTicket.firstSeatsAdults,
+                businessSeatsAdults: prevTicket.businessSeatsAdults,
+                economySeatsAdults: prevTicket.economySeatsAdults,
+                firstSeatsChildren: prevTicket.firstSeatsChildren,
+                businessSeatsChildren: prevTicket.businessSeatsChildren,
+                economySeatsChildren: prevTicket.economySeatsChildren
+            }
+            await axios.patch('http://localhost:8082/api/flights/flightcancelseats/', token_allold_seatlists_flightid)
+            if (prevTicket.departureTicket) {
+                const token_oldTicket_reservNo = {
+                    token: sessionStorage.getItem('token'),
+                    departureTicket: prevTicket.departureTicket,
+                    reservationNumber: prevTicket.resNo
+                }
+                await axios.patch('http://localhost:8082/api/reservations/cancelhalfreservation/', token_oldTicket_reservNo)
+                    .catch(err => console.log(err))
+
+                const token_newTicket_reservNo = {
+                    token: sessionStorage.getItem('token'),
+                    departureTicket: newTicket,
+                    reservationNumber: prevTicket.resNo
+                }
+                await axios.patch('http://localhost:8082/api/reservations/bookhalfreservation/', token_newTicket_reservNo)
+                    .catch(err => console.log(err))
+            }
+            else if (prevTicket.returnTicket) {
+                const token_oldTicket_reservNo = {
+                    token: sessionStorage.getItem('token'),
+                    returnTicket: prevTicket.returnTicket,
+                    reservationNumber: prevTicket.resNo
+                }
+                await axios.patch('http://localhost:8082/api/reservations/cancelhalfreservation/', token_oldTicket_reservNo)
+                    .catch(err => console.log(err))
+
+                const token_newTicket_reservNo = {
+                    token: sessionStorage.getItem('token'),
+                    returnTicket: newTicket,
+                    reservationNumber: prevTicket.resNo
+                }
+                await axios.patch('http://localhost:8082/api/reservations/bookhalfreservation/', token_newTicket_reservNo)
+                    .catch(err => console.log(err))
+            }
+            const token_newSeats = {
+                token: sessionStorage.getItem('token'),
+                flightId: newTicket.flight,
+                firstSeatsAdults: newTicket.firstSeatsAdults,
+                businessSeatsAdults: newTicket.businessSeatsAdults,
+                economySeatsAdults: newTicket.economySeatsAdults,
+                firstSeatsChildren: newTicket.firstSeatsChildren,
+                businessSeatsChildren: newTicket.businessSeatsChildren,
+                economySeatsChildren: newTicket.economySeatsChildren
+            }
+
+            await axios.patch('http://localhost:8082/api/flights/flightbookseats/', token_newSeats)
+                .catch(err => console.log(err))
+        }
 
         const authChannel = new BroadcastChannel("auth")
         authChannel.postMessage({ success: true })
